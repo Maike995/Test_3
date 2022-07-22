9c0a23d54c6326222c75e0b6de158000d5098ac0
Miichael <105318393+Maike995@users.noreply.github.com>
2022-07-22 19:02:29 +0000

    N faço ideia de como fazer a 7-8

--- Main.java
+++ Main.java
@@ -1,4 +1,22 @@
 /*
+Crie uma classe Funcionario que contenha os atributos matricula, nome, sgEstadoNascimento, sgDepartamento,
+vlSalarioBase, vlSalarioLiquido, txINSS, qtFilhos e o método abstrato calculaSalario. Para cada filho considere um auxílio
+de R$ 100.00. Considerações:
+
+01 - Implemente um construtor na classe Funcionario com os parâmetros matricula, nome, sgEstadoNascimento,
+sgDepartamento, vlSalarioBasel. Crie 2 classes que herdam de Funcionario (FuncionarioCLT e FuncionarioTemp).
+02 - A classe FuncionarioCLT deve conter o atributo vlSalarioProdutividade e o construtor deve herdar da classe pai
+adicionando o vlSalarioProdutividade (R$ 500,00) e txINSS (20%).
+03 - A classe FuncionarioCLT deve implementar o método calculaSalario com o objetivo de calcular o vlSalarioLiquido
+(vlSalarioBase + vlAuxilioFilhos + vlSalarioProd) descontando a txINSS.
+04 - A classe FuncionarioCLT deve implementar também o método calculaAumentoSalarial com 2 assinaturas diferentes.
+Uma implementação deve conter (nesta ordem) os argumentos matrícula e o percentual de aumento (%) e a segunda
+implementação os argumentos valor de aumento (R$) e matrícula.
+
+05 - A classe FuncionarioTemp deve conter o atributo tempoContrato e o construtor deve herdar da classe pai
+adicionando o tempo de contrato (12 meses) e txINSS (10%).
+06 - A classe FuncionarioTemp deve implementar o método calculaSalario com o objetivo de calcular o vlSalarioLiquido
+(vlSalarioBase + vlAuxilioFilhos) descontando a txINSS.
 07 - Crie uma classe para testar as classes criadas acima com 3 objetos da classe FuncionarioCLT (matrículas 1, 2, 3) e
 3 objetos da classe FuncionarioTemp (matrículas 4, 5, 6). Adicione em um vetor conforme a aula do dia 11/07 do
 material: 22 - s03e16 - Operador InstanceOf.
